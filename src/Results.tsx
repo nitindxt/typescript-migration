b7f3e31e1dbcf01a1c5c69f96af6930931c8a7d5
Nitin Dixit <nitinajaydixit@gmail.com>
2022-04-04 14:06:46 +0530

    migrated Result.js to .tsx

--- src/Results.tsx
+++ src/Results.tsx
@@ -1,6 +1,8 @@
+import { FunctionComponent } from "react";
+import {Pet as PetType} from "./APIResponsesTypes";//doing this bcz we have file Pet and also interface Pet
 import Pet from "./Pet";
 
-const Results = ({ pets }) => {
+const Results: FunctionComponent<{pets: PetType[]}> = ({ pets  }) => {
   return (
     <div className="search">
       {!pets.length ? (
