45aa6bf82b7371fc322d8d8865cf4951ea237dda
Nitin Dixit <nitinajaydixit@gmail.com>
2022-04-04 13:46:19 +0530

    migrated useBreedlist to .ts and mod APIResponseTypes to add BreedListResponse interface

---
+++ src/useBreedList.ts
@@ -0,0 +1,40 @@
+import { useState, useEffect } from "react";
+import { Animal, BreedListAPIResponse } from "./APIResponsesTypes";
+
+const localCache: {
+[index: string]: string[]
+} = {};
+
+type Status = "unloaded" | "loading" | "loaded";
+
+export default function useBreedList(animal: Animal) {
+  const [breedList, setBreedList] = useState([] as string[]);
+  const [status, setStatus] = useState("unloaded" as Status);
+
+  useEffect(() => {
+    if (!animal) {
+      setBreedList([]);
+    } else if (localCache[animal]) {
+      setBreedList(localCache[animal]);
+    } else {
+      void requestBreedList();//void js keyword tells that requestBreedList() promise can return anything|undefined, so ignore
+    }
+
+    async function requestBreedList() {
+      setBreedList([]);
+      setStatus("loading");
+      if(!animal){
+        return;
+      }
+      const res = await fetch(
+        `http://pets-v2.dev-apis.com/breeds?animal=${animal}`
+      );
+      const json = (await res.json()) as BreedListAPIResponse;
+      localCache[animal] = json.breeds || [];
+      setBreedList(localCache[animal]);
+      setStatus("loaded");
+    }
+  }, [animal]);
+
+  return [breedList, status];
+}
